67d7db1261bb09594410f92a81534a05bd228b0b
tntmancer <tntmancer@gmail.com>
2024-06-14 19:33:01 -0400

    Added replies and posts to store. I think we will have communities be static (no reducer) like Kanbas courses.

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import profilesReducer from "./Profile/ProfileReducer";
+import repliesReducer from "./Replies/reducer";
+import postsReducer from "./Posts/reducer";
 
 const store = configureStore({
   reducer: {
-    profilesReducer
-  },
+    profilesReducer,
+    repliesReducer,
+    postsReducer,
+  }
 });
 export default store;
